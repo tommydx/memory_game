1e8f7cd3df730bca34778b1255c20b0a123c8dfd
tommydx <thomas.docu0@gmail.com>
2017-04-12 02:19:25 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
  console.log("Up and running!");
 
- var cardOne = "queen";
- var cardTwo = "queen";
- var cardThree = "king";
- var cardFour = "king";
- console.log("User flipped" + " " + cardOne + " " + cardFour);
+ var cards = ['queen', 'queen', 'king', 'king'];
+ var cardsInPlay = [];
+
+
+	var checkForMatch = function () {
+
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+  	    console.log("You found a match!");
+ 		 } 
+ 		else {
+     	console.log("Sorry, try again.");
+ 		 }
+	}
+
+	
+	var flipCard = function (cardId) {
+		
+
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+
+	}
+
+
+	flipCard(0);
+	flipCard(2);
