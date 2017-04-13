8f4304870b402ab39256e5fa2b886cddb1218413
tommydx <thomas.docu0@gmail.com>
2017-04-13 15:03:46 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,25 +1,50 @@
  console.log("Up and running!");
 
- var cards = ['queen', 'queen', 'king', 'king'];
+ var cards = [
+ 		{
+ 			rank: 'Queen',
+ 			suit: 'Hearts',
+ 			cardImage: "images/queen-of-hearts.png"
+ 		},
+ 		{
+ 			rank: 'Queen',
+ 			suit: 'Diamonds',
+ 			cardImage: "images/queen-of-diamonds.png"
+ 		},
+ 		{
+ 			rank: 'King',
+ 			suit: 'Hearts',
+ 			cardImage: "images/king-of-hearts.png"
+ 		},
+ 		{
+ 			rank: 'King',
+ 			suit: 'Diamonds',
+ 			cardImage: "images/king-of-diamonds.png"
+ 		}
+
+ ];
  var cardsInPlay = [];
 
 
 	var checkForMatch = function () {
 
 		if (cardsInPlay[0] === cardsInPlay[1]) {
-  	    console.log("You found a match!");
+  	    alert("You found a match!");
  		 } 
  		else {
-     	console.log("Sorry, try again.");
+     	alert("Sorry, try again.");
  		 }
 	}
 
 	
 	var flipCard = function (cardId) {
 		
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	
 	checkForMatch();
 
 	}
